7da61fc37c2bd24384d8a5468c6d3d41fb3ee86b
ivadzz <vdavi205@gmail.com>
2024-04-25 15:42:20 -0300

    newcommit banco

--- src/bancoPCD/Banco.java
+++ src/bancoPCD/Banco.java
@@ -2,13 +2,13 @@ package bancoPCD;
 
 public class Banco {
     public static void main(String[] args) throws InterruptedException {
-        //inicia as lojas
+        //INICIA AS LOJAS
         Loja[] lojas = new Loja[2]; 
         for (int l = 0; l < lojas.length; l++) {
             lojas[l] = new Loja(0); 
         }
 
-        // "" "" funcionarios
+        // "" "" FUNCIONARIOS
         Funcionario[] funcionarios = new Funcionario[4]; 
         for (int k = 0; k < funcionarios.length; k++) {
             Conta salarioLoja = lojas[k / 2].getConta(); 
@@ -16,21 +16,21 @@ public class Banco {
             funcionarios[k] = new Funcionario(salarioLoja, contaInvestimento);
             funcionarios[k].start();
         }
-        // "" "" contas
+        // "" "" CONTAS
         Conta[] contasClientes = new Conta[5]; 
         for (int j = 0; j < contasClientes.length; j++) {
-        //cada conta coemça com 1000
+        //AS CONTAS COMEÇAM COM 1000
             contasClientes[j] = new Conta(1000); 
         }
         
-        // inicia os clientes
+        // INICIA OS CLIENTES
         Cliente[] clientes = new Cliente[5]; 
         for (int i = 0; i < clientes.length; i++) {
             clientes[i] = new Cliente(contasClientes[i], lojas);
             clientes[i].start();
         }
 
-        //coloca as threads pra rodar
+        
         for (Cliente cliente : clientes) {
             try {
                 cliente.join();
@@ -39,7 +39,7 @@ public class Banco {
             }
         }
 
-        // PAG das lojas
+        // PAG DAS LOJAS
         for (Loja loja : lojas) {
             loja.pagarSalarios(); 
         }
@@ -50,14 +50,14 @@ public class Banco {
             System.out.println("---------------------------------");
             System.out.println("O cliente " + (index + 1) + " restou --> R$" + contasClientes[index].getSaldo());
             System.out.println("---------------------------------");
-            Thread.sleep(1000); 
+            Thread.sleep(1000); //DELAY
 
         }
         for (int index = 0; index < lojas.length; index++) {
             System.out.println("\n\n---------------------------------");
             System.out.println("Loja " + (index + 1) + " faturou --> R$" + lojas[index].getConta().getSaldo());
             System.out.println("---------------------------------");
-            Thread.sleep(1000); 
+            Thread.sleep(1000); //DELAY
         }
         for (int h = 0; h < funcionarios.length; h++) {
             System.out.println("\n===========================================================================\nO funcionário " + (h + 1) + ", da loja " + (h / 2 + 1) + ", obteve de Salário --> R$" + (funcionarios[h].getSalario().getSaldo()/2+1400) + ", e investiu--> R$" + (funcionarios[h].getSalario().getSaldo()/2+1400)*0.2);
