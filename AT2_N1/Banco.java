f1904f4486f5dc427d619efa350de040ccaa5874
ivadzz <vdavi205@gmail.com>
2024-04-24 23:30:04 -0300

    new commit

--- AT2_N1/Banco.java
+++
@@ -1,9 +0,0 @@
-public class Banco {
-    public synchronized boolean transferir(Conta de, Conta para, double valor) {
-        if (de.retirar(valor)) {
-            para.depositar(valor);
-            return true;
-        }
-        return false;
-    }
-}
